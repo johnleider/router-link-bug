0623cde12ab5f0a1ed39f3288300416d2881140c
John Leider <john.j.leider@gmail.com>
2019-05-27 12:27:03 -0500

    chore(update bug display):

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
   <div>
-		<button @click="show = !show">Open</button>
     <router-link to="/" v-if="show">
+      Router Link
       <input type="password">
     </router-link>
+
+    <div></div>
+
+    <test v-if="show" @click="click">
+      Custom component w/ Render FN
+      <input type="password">
+    </test>
 	</div>
 </template>
 
 <script>
   export default {
+    components: {
+      Test: {
+        methods: {
+          click () {}
+        },
+
+        render (h) {
+          return h('div', {
+            on: {
+              click: this.click
+            }
+          }, this.$slots.default)
+        }
+      }
+    },
+
     data () {
       return {
         show: false
       }
+    },
+
+    mounted () {
+      setInterval(() => {
+        this.show = !this.show
+      }, 2000)
+    },
+
+    methods: {
+      click () {}
     }
   }
 </script>
